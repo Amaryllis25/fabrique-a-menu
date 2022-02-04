06a71334754dd1e4843dabe6e04da706a37d55e5
Amaryllis25 <amaryllis.vignaud@ensg.eu>
2022-02-04 10:28:02 +0100

    Fix: First attempt hello world

--- src/main/java/com/cicdlectures/menuserver/MenuServerApplication.java
+++ src/main/java/com/cicdlectures/menuserver/MenuServerApplication.java
@@ -1,13 +1,16 @@
 package com.cicdlectures.menuserver;
 
+import com.cicdlectures.menuserver.service.DeleteMenuService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import picocli.CommandLine;
 
 @SpringBootApplication
 public class MenuServerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MenuServerApplication.class, args);
+    CommandLine.run(new DeleteMenuService(), args);
   }
 
 }
